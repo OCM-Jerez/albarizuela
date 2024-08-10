d4bcde823d6116bb2d12d3fad70a294f0f529d4b
mamjerez <correomam@gmail.com>
2024-08-10 16:43:50 +0200

    Add casas-ocupadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CasasOcupadasComponent } from './pages/casas-ocupadas/casas-ocupadas.component';
 import { ConteneroresBizcocherosComponent } from './pages/contenerores-bizcocheros/contenerores-bizcocheros.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MapComponent } from './pages/map/map.component';
 import { TableProblemasComponent } from './pages/table-problemas/table-problemas.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'table', component: TableProblemasComponent },
-  { path: 'map', component: MapComponent },
-  { path: 'contenedores-bizcocheros', component: ConteneroresBizcocherosComponent },
+	{ path: 'home', component: HomeComponent },
+	{ path: 'table', component: TableProblemasComponent },
+	{ path: 'map', component: MapComponent },
+	{ path: 'contenedores-bizcocheros', component: ConteneroresBizcocherosComponent },
+	{ path: 'casas-ocupadas', component: CasasOcupadasComponent },
 
-  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+	{ path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/assets/markers/casas-ocupadas.ts
@@ -0,0 +1,19 @@
+export const casasOcupadas = [
+    {
+        title: `<h1>Calle Rosario 5</h1>
+        <h3>Mujer anciana propietaria reside en ella</h3>
+        <h4>Ocupada</h4>
+        <img src="https://res.cloudinary.com/dabrencx7/image/upload/v1723297519/rosario5_x1nmox.jpg">
+       `,
+        lat: 36.68690,
+        long: -6.13585,
+    },
+    {
+        title: `<h1>Calle Don Juan</h1>
+        <h3></h3>
+        <h4></h4>
+        <img src="https://res.cloudinary.com/dabrencx7/image/upload/v1723297519/donJuan_yyx2y1.jpg">`,
+        lat: 36.68455,
+        long: -6.13185,
+    }
+]
